69a1d6af623065c37212dc334f4198071096db61
japiirainen <joona.piirainen@gmail.com>
2024-06-29 10:11:02 +0300

    chore: clean up 002

--- 001/main.c
+++ 001/main.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
-int main() {
+int main(void) {
   int i;
   long long sum = 0;
+
   for (i = 3; i < 1000; ++i)
     if (i % 3 == 0 || i % 5 == 0)
       sum += i;
+
   printf("%lld", sum);
+
+  return 0;
 }
