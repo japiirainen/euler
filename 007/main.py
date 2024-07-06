cea9b2e982b410c114cbb833e2d812d3c295dfbd
japiirainen <joona.piirainen@gmail.com>
2024-07-06 15:25:04 +0300

    feat(007): add python solution

--- 007/main.py
+++ 007/main.py
@@ -1,1 +1,15 @@
-print("not implemented.")
+n = int(10e6)
+target = 10001
+prime_count = 0
+
+sieve = [True] * n
+sieve[0] = sieve[1] = False
+
+for i in range(n):
+    if sieve[i]:
+        prime_count += 1
+        if prime_count == target:
+            print(i)
+            break
+        for j in range(i * 2, len(sieve), i):
+            sieve[j] = False
