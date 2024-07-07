3f1b97b9c45ec3194e9c0ce46db68a063e165abc
japiirainen <joona.piirainen@gmail.com>
2024-07-07 15:40:53 +0300

    feat(009): add python solution

--- 009/main.py
+++ 009/main.py
@@ -1,1 +1,6 @@
-print("not implemented")
+for a in range(1, 1001):
+    for b in range(a + 1, 1001):
+        c = 1000 - a - b
+        if a**2 + b**2 == c**2:
+            print(a * b * c)
+            break
