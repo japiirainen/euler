25eb13c9b124810f3aa43153c14af31337632b13
japiirainen <joona.piirainen@gmail.com>
2024-07-08 18:02:59 +0300

    feat(010): add python solution

--- 010/main.py
+++ 010/main.py
@@ -1,1 +1,14 @@
-print("not implemented.")
+N = int(2e6)
+
+
+def sieve():
+    xs = [True] * N
+    xs[0] = xs[1] = False
+    for i in range(2, N):
+        if xs[i]:
+            for j in range(i * 2, N, i):
+                xs[j] = False
+    return xs
+
+
+print(sum(i for i, is_prime in enumerate(sieve()) if is_prime))
