981ef8545dd56d069b271d84eee69c2c752580d7
japiirainen <joona.piirainen@gmail.com>
2024-07-13 11:23:15 +0300

    feat(012): add c solution

--- 012/main.c
+++ 012/main.c
@@ -1,6 +1,28 @@
+#include <math.h>
 #include <stdio.h>
 
+unsigned num_divisors(unsigned long n) {
+  unsigned d = 0;
+  unsigned long i;
+
+  for (i = 1; i <= sqrt(n); i++) {
+    if (n % i == 0) {
+      d += 2;
+    }
+  }
+
+  return d * d == n ? d - 1 : d;
+}
+
+unsigned long triangle(unsigned n) { return (n * (n + 1)) / 2; }
+
 int main(void) {
-  printf("not implemented.\n");
+  unsigned long i = 1;
+
+  while (num_divisors(triangle(i)) <= 500)
+    i++;
+
+  printf("%lu\n", triangle(i));
+
   return 0;
 }
