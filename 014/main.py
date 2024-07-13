776c5b9a81fbf70b4bc7c2468bbf7ffac8849e6e
japiirainen <joona.piirainen@gmail.com>
2024-07-13 17:45:53 +0300

    refactor(014): small cleanup on python solution

--- 014/main.py
+++ 014/main.py
@@ -3,9 +3,7 @@ import functools
 
 @functools.cache
 def collatz(n: int) -> int:
-    if n == 1:
-        return 1
-    return collatz(n // 2 if n % 2 == 0 else n * 3 + 1) + 1
+    return collatz(n // 2 if n % 2 == 0 else n * 3 + 1) + 1 if n != 1 else 1
 
 
 print(max(((i, collatz(i)) for i in range(1, int(10e5))), key=lambda x: x[1])[0])
