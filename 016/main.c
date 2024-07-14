ce7e955b8da79141bc0128f909bb0bd9648bd026
japiirainen <joona.piirainen@gmail.com>
2024-07-14 09:04:14 +0300

    feat(016): add solution in c, python and mathematica

---
+++ 016/main.c
@@ -0,0 +1,26 @@
+#include <gmp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  mpz_t n;
+  char *str;
+  size_t len, i;
+  unsigned sum = 0;
+
+  mpz_init(n);
+  mpz_ui_pow_ui(n, 2, 1000);
+
+  str = mpz_get_str(NULL, 10, n);
+  len = mpz_sizeinbase(n, 10);
+
+  for (i = 0; i < len; i++)
+    sum += str[i] - '0';
+
+  printf("%u\n", sum);
+
+  free(str);
+  mpz_clear(n);
+
+  return 0;
+}
