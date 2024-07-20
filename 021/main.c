43df8093ad7e0c19106d68360312a744c8295e18
japiirainen <joona.piirainen@gmail.com>
2024-07-20 15:22:23 +0300

    feat(021): add c solution

--- 021/main.c
+++ 021/main.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
 
+unsigned divisorsum(unsigned n) {
+  unsigned sum = 1;
+  size_t k = n;
+
+  size_t i;
+
+  for (i = 2; i <= k; i++) {
+    int p = 1;
+
+    while (k % i == 0) {
+      p *= i;
+      k /= i;
+    }
+
+    sum *= (p * i - 1) / (i - 1);
+  }
+
+  return sum - n;
+}
+
 int main() {
-  printf("not implemented.\n");
+  unsigned i, d, sum = 0;
+
+  for (i = 2; i < 10000; i++) {
+    d = divisorsum(i);
+    if (i < divisorsum(i) && divisorsum(d) == i)
+      sum += i + d;
+  }
+
+  printf("%u\n", sum);
 
   return 0;
 }
