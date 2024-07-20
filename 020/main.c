e6080388a55ad0d404b1fbcf910fa1034f57b015
japiirainen <joona.piirainen@gmail.com>
2024-07-20 14:48:12 +0300

    feat(020): add c solution

--- 020/main.c
+++ 020/main.c
@@ -3,7 +3,24 @@
 #include <stdlib.h>
 
 int main() {
-  printf("not implemented.\n");
+  mpz_t n;
+
+  mpz_init(n);
+  mpz_fac_ui(n, 100);
+
+  char *out;
+  out = mpz_get_str(NULL, 10, n);
+
+  unsigned sum = 0;
+
+  size_t i;
+  for (i = 0; out[i]; i++)
+    sum += out[i] - '0';
+
+  printf("%u\n", sum);
+
+  free(out);
+  mpz_clear(n);
 
   return 0;
 }
