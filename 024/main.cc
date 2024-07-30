10e2a45823ee369e246eb0419df666626fc3c070
japiirainen <joona.piirainen@gmail.com>
2024-07-30 16:37:52 +0200

    feat(024): add cpp solution

--- 024/main.cc
+++ 024/main.cc
@@ -3,7 +3,14 @@
 using namespace std;
 
 int main(void) {
-  cout << "not implemented." << endl;
+  unsigned int n = 10e5;
+
+  string cur = "0123456789";
+
+  while (--n)
+    next_permutation(cur.begin(), cur.end());
+
+  cout << cur << endl;
 
   return 0;
 }
