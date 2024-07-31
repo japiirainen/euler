92b8564aef7ae08f1ee6fe68fb6a24b76524ebcc
japiirainen <joona.piirainen@gmail.com>
2024-07-31 17:13:33 +0200

    chore(025): simplify python solution

--- 025/main.py
+++ 025/main.py
@@ -6,10 +6,9 @@ def fib(n: int) -> int:
     return fib(n - 1) + fib(n - 2) if n > 2 else 1
 
 
-n_digits, n = 1, 1
+n = 1
 
-while n_digits < 1000:
+while fib(n) < 10**999:
     n += 1
-    n_digits = len(list(str(fib(n))))
 
 print(n)
