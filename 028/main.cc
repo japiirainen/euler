5ee14ca4a080bb557e28d0e8eb3a40ea3b6dd244
japiirainen <joona.piirainen@gmail.com>
2024-08-05 14:19:19 +0200

    feat(028): add python solution

---
+++ 028/main.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+
+
+int main(void) {
+  std::cout << "not implemented." << std::endl;
+
+  return 0;
+}
---
+++ 028/main.py
@@ -0,0 +1,3 @@
+W = 1001
+
+print(1 + sum((4 * (i**2)) - (6 * (i - 1)) for i in range(3, W + 1, 2)))
