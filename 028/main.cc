172d3fd61d037fad2ffd3a63625433475edc8297
japiirainen <joona.piirainen@gmail.com>
2024-08-05 14:26:29 +0200

    feat(028): add cpp solution

--- 028/main.cc
+++ 028/main.cc
@@ -1,8 +1,18 @@
 #include <iostream>
 
+const unsigned int WIDTH = 1001;
 
 int main(void) {
-  std::cout << "not implemented." << std::endl;
+  unsigned long long sum = 0;
+
+  // first 'level'
+  sum += 1;
+
+  for (unsigned int i = 3; i <= WIDTH; i += 2) {
+    sum += (4 * (i * i)) - (6 * (i - 1));
+  }
+
+  std::cout << sum << std::endl;
 
   return 0;
 }
