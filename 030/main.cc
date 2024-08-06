78f80e6aaa4de0f3a4d87914b7b532cdf5b7e5b3
japiirainen <joona.piirainen@gmail.com>
2024-08-06 13:07:06 +0200

    feat(030): add cpp solution

--- 030/main.cc
+++ 030/main.cc
@@ -1,7 +1,36 @@
+#include <algorithm>
 #include <iostream>
+#include <numeric>
+#include <vector>
+
+std::vector<int> integer_digits(int n) {
+  std::vector<int> digits;
+  int x = n;
+
+  while (x) {
+    digits.push_back(x % 10);
+    x /= 10;
+  }
+
+  std::reverse(digits.begin(), digits.end());
+  return digits;
+}
+
+int fdps(int n) {
+  auto digits = integer_digits(n);
+  std::transform(digits.begin(), digits.end(), digits.begin(),
+                 [](int d) { return pow(d, 5); });
+  return std::accumulate(digits.begin(), digits.end(), 0);
+}
 
 int main(void) {
-  std::cout << "not implemented." << std::endl;
+  int sum = 0;
+
+  for (int n = 2; n < 10e5; n++)
+    if (n == fdps(n))
+      sum += n;
+
+  std::cout << sum << std::endl;
 
   return 0;
 }
