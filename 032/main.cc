babc9c0c72a64b0dd4f3922fb69f3264acc555ee
japiirainen <joona.piirainen@gmail.com>
2024-08-18 13:52:27 +0300

    feat(032): add cpp solution

--- 032/main.cc
+++ 032/main.cc
@@ -1,7 +1,28 @@
+#include <algorithm>
 #include <iostream>
+#include <string>
+
+bool pred(int n) {
+  for (int k = 1; k <= sqrt(n); k++)
+    if (n % k == 0) {
+      std::string s =
+          std::to_string(n) + std::to_string(k) + std::to_string(n / k);
+      std::sort(s.begin(), s.end());
+      if (s == "123456789")
+        return true;
+    }
+
+  return false;
+}
 
 int main(void) {
-  std::cout << "not implemented." << std::endl;
+  int sum = 0;
+
+  for (int i = 1; i < 10e4; i++)
+    if (pred(i))
+      sum += i;
+
+  std::cout << sum << std::endl;
 
   return 0;
 }
